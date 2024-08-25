c882f0bae2c92e631777842c712d2ce4a4493ea3
Angela Holden <angelajholden@gmail.com>
2024-08-25 15:23:23 -0500

    search modal/drawer styles

--- main.js
+++ main.js
@@ -15,6 +15,27 @@ function clickMenu() {
 	});
 }
 
+function searchOpen() {
+	const icon = document.querySelector(".search");
+	const drawer = document.querySelector(".search-drawer");
+	const body = document.querySelector("body");
+
+	icon.addEventListener("click", (e) => {
+		const clicked = e.target.classList.contains("active");
+
+		if (clicked) {
+			icon.classList.remove("active");
+			drawer.classList.remove("active");
+			body.classList.remove("active");
+		} else {
+			icon.classList.add("active");
+			drawer.classList.add("active");
+			body.classList.add("active");
+		}
+	});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	clickMenu();
+	searchOpen();
 });
